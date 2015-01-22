4df49b51e41914e71aa302de79bfda412dc63c39
Alex <96dalex@gmail.com>
2015-01-21 18:56:21 -0700

    small improvements on views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session.clear
-    redirect_to root_path, notice: "You have been logged out"
+    redirect_to root_path
   end
 
   protected
