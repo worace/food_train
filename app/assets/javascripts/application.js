49dabfae32ad3bb963f1194859b25fb5e3e91eca
Alex <96dalex@gmail.com>
2015-01-22 11:16:57 -0700

    tooltip is working ish

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,6 @@
 //= require turbolinks
 //= require_tree .
 //= require bootstrap
+$(function() {
+  $('.vote-item-tooltip').tooltip();
+});
--- app/models/train_option.rb
+++ app/models/train_option.rb
@@ -9,4 +9,11 @@ class TrainOption < ActiveRecord::Base
   def yelp_business
     @yelp_business ||= YelpReader.search_for_restaurant(place)
   end
+
+  def yelp_content
+    "Rating:  #{yelp_business.rating}
+    Phone:  #{yelp_business.display_phone}
+    Address:  #{yelp_business.location.address.first}
+    Category: #{yelp_business.categories.first.first}"
+  end
 end
